69c93796e19ed2be1503e887bdb722b306ac5318
Mejdi <saidmejdi123@gmail.com>
2020-08-02 11:23:58 +0100

    Updates

---
+++ precache-manifest.2338f0105e78eca5521a55e90083740d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d23d76172c3644bf11afc6d47830321",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "e0b03b021a908b6f323e",
+    "url": "/covid-dashboard/static/css/main.9576fcc9.chunk.css"
+  },
+  {
+    "revision": "e6cff9c0e8e5d48e26dd",
+    "url": "/covid-dashboard/static/js/2.502d500a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-dashboard/static/js/2.502d500a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b03b021a908b6f323e",
+    "url": "/covid-dashboard/static/js/main.6dac1761.chunk.js"
+  },
+  {
+    "revision": "f3609f817b855312420d",
+    "url": "/covid-dashboard/static/js/runtime-main.5b51f784.js"
+  }
+]);
